6a8440f48b7f9700a4383bc214aa7d078b7ad689
Tomas <arnotomas1@gmail.com>
2022-12-07 12:28:05 -0300

    Forma Marce

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 function mostrarResultados(obj) {
-  const items = obj.items;
-  const imgData = obj.includes.Asset;
-
   const templateEl = document.querySelector(".template").content;
   const contenedorEl = document.querySelector(".results");
 
@@ -10,29 +7,39 @@ function mostrarResultados(obj) {
   const descriptionEl = templateEl.querySelector(".proyect-description");
   const linkEl = templateEl.querySelector(".see-more");
 
-  let contador = 0;
-  while (contador < items.length) {
-    imgEl.setAttribute("src", imgData[contador].fields.file.url);
-    titleEl.textContent = items[contador].fields.titulo;
-    descriptionEl.textContent = items[contador].fields.descripcion;
-    linkEl.setAttribute("href", items[contador].fields.url);
+  // imgEl.setAttribute("src", imgData[contador].fields.file.url);
+  titleEl.textContent = obj.titulo;
+  descriptionEl.textContent = obj.descripcion;
+  linkEl.setAttribute("href", obj.url);
 
-    const clone = document.importNode(templateEl, true);
-    contenedorEl.appendChild(clone);
-    contador++;
-  }
+  const clone = document.importNode(templateEl, true);
+  contenedorEl.appendChild(clone);
 }
 
 function parseador() {
-  fetch(
+  return fetch(
     "https://cdn.contentful.com/spaces/xfz9lznsmed7/environments/master/entries?access_token=5qriaqQtppfoNJR67S_Qv2ZKGPR3xA456sGwZj23GPM"
-  ).then((obj) => {
-    obj.json().then((res) => {
-      mostrarResultados(res);
+  )
+    .then((obj) => {
+      return obj.json();
+    })
+    .then((data) => {
+      const fieldsColl = data.items.map((item) => {
+        return {
+          titulo: item.fields.titulo,
+          descripcion: item.fields.descripcion,
+          url: item.fields.url,
+        };
+      });
+      return fieldsColl;
     });
+}
+function main() {
+  parseador().then((items) => {
+    for (const item of items) {
+      mostrarResultados(item);
+    }
   });
 }
-
-function main() {}
 
 main();
